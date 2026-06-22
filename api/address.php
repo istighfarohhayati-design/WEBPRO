1900bc78fc8817d4ff3d79d6950af2f547c812b4
Fachira Selima <fachira.selima05@gmail.com>
2026-06-22 17:52:42 +0700

    uji coba

---
+++ api/address.php
@@ -0,0 +1,28 @@
+<?php
+// api/address.php
+include '../config.php';
+header("Content-Type: application/json");
+
+$buyer = checkAPIKey();
+$buyer_id = $buyer['id'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'GET': // Method 1
+        $res = mysqli_query($conn, "SELECT * FROM buyer_addresses WHERE buyer_id = $buyer_id");
+        echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+        break;
+
+    case 'POST': // Method 2
+        $data = json_decode(file_get_contents("php://input"), true);
+        $label = mysqli_real_escape_string($conn, $data['label']);
+        $detail = mysqli_real_escape_string($conn, $data['detail_address']);
+        mysqli_query($conn, "INSERT INTO buyer_addresses (buyer_id, label, detail_address) VALUES ($buyer_id, '$label', '$detail')");
+        echo json_encode(["message" => "Alamat berhasil ditambahkan"]);
+        break;
+
+    default:
+        http_response_code(405);
+        break;
+}
+?>
---
+++ api/profile_photo.php
@@ -0,0 +1,26 @@
+<?php
+// api/profile_photo.php
+include '../config.php';
+header("Content-Type: application/json");
+
+$buyer = checkAPIKey();
+$buyer_id = $buyer['id'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'GET': // Method 1
+        echo json_encode(["photo_url" => $buyer['photo_url']]);
+        break;
+
+    case 'POST': // Method 2
+        $data = json_decode(file_get_contents("php://input"), true);
+        $photo = mysqli_real_escape_string($conn, $data['photo_url']);
+        mysqli_query($conn, "UPDATE buyers SET photo_url='$photo' WHERE id=$buyer_id");
+        echo json_encode(["message" => "Foto profil berhasil diperbarui"]);
+        break;
+
+    default:
+        http_response_code(405);
+        break;
+}
+?>
